fb129be24bc0f84467e357079cb8687bf4fa076d
cooperbrindle <cooperbrindle96@gmail.com>
2021-05-02 21:13:19 +1000

    add test_readTicker

--- test_marketStack.py
+++ test_marketStack.py
@@ -1,5 +1,12 @@
 import unittest
 import marketStack
 
+
 def test_checkProfile():
     assert marketStack.checkProfile("GarbageProfile") == False
+
+
+def test_readTicker(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "WTC.XASX")
+    ticker = marketStack.readTicker()
+    assert ticker == "WTC.XASX"
