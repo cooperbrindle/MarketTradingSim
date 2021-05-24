d095d6bae706b63e24292cb5204148f1ad20728b
cooperbrindle <cooperbrindle96@gmail.com>
2021-05-24 18:42:20 +1000

    add test stubs

--- test_main.py
+++ test_main.py
@@ -1,13 +1,24 @@
-import unittest
 import main
-import sys
 
 def test_inputAction(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "help")
-    action = main.inputAction()
-    assert action == "help"
+	monkeypatch.setattr('builtins.input', lambda _: "help")
+	action = main.inputAction()
+	assert action == "help"
+
+def test_menuControl():
+	pass
+
+def test_addTicker():
+	pass
+
+def test_inputTicker():
+	pass
+
+def test_printExchanges():
+	pass
 
 def test_showOptions(capsys):  # or use "capfd" for fd-level
-    main.showOptions()
-    captured = capsys.readouterr()
-    assert captured.out == "help: Show available actions\n" + "add-ticker: Collect data on a company\n" + "exchanges: Print list of available exchanges\n" + "close: Close program\n"
+	main.showOptions()
+	captured = capsys.readouterr()
+	assert captured.out == "help: Show available actions\n" + "add-ticker: Collect data on a company\n" + \
+		"exchanges: Print list of available exchanges\n" + "close: Close program\n"
