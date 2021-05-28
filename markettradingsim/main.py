069d5c571ebd1df4646d5c7fe7005ff9157b2b4c
cooperbrindle <cooperbrindle96@gmail.com>
2021-05-28 23:28:09 +1000

    Add GUI

--- markettradingsim/main.py
+++ markettradingsim/main.py
@@ -1,69 +1,66 @@
 import re
+import tkinter as tk
 from markettradingsim.constants import MENU_CONSTANTS, TEXTONLY
 import markettradingsim.marketStack as api
 import markettradingsim.databaseActions as DB
 
-def main():
-	userInput = inputAction()
-	while (userInput != 'close'):
-		menuControl(userInput)
-		userInput = inputAction()
-
-def inputAction():
-	action = input(MENU_CONSTANTS["WELCOME"])
-	return action
-
-def menuControl(userInput):
-	if userInput == 'help':
-		showOptions()
-	elif userInput == 'add-ticker':
-		ticker = inputTicker()
-		api_response = api.getTicker(ticker, 'eod')
-		if api.validateResponse(api_response):
-			addTicker(api_response["data"])
-	elif userInput == 'exchanges':
-		api_response = api.getAncillaryData(userInput)
-		if api.validateResponse(api_response):
-			printExchanges(api_response["data"])
+def inputTicker(entries):
+	stockCode = entries['Stock Code'].get().upper()
+	exchange = entries['Exchange'].get().upper()
+	if re.match(TEXTONLY["REGEX"], exchange) and re.match(TEXTONLY["REGEX"], stockCode):
+		if exchange == 'XNAS':
+			ticker = stockCode
+		else:
+			ticker = stockCode + '.' + exchange
+		addTicker(ticker)
 	else:
-		print(MENU_CONSTANTS["INVALID"])
+		print(TEXTONLY["ERROR"])
 
 def addTicker(ticker):
-	key = DB.checkProfile(ticker["symbol"])
-	if key == False:
-		key = DB.createProfile(ticker)
-	DB.insertData(ticker["eod"], key)
+	api_response = api.getTicker(ticker, 'eod')
+	if api.validateResponse(api_response):
+		key = DB.checkProfile(api_response["data"]["symbol"])
+		if key == False:
+			key = DB.createProfile(api_response["data"])
+		DB.insertData(api_response["data"]["eod"], key)
 
-def inputTicker():
-	while True:
-		symbol = input("Please enter stock code: ")
-		if not re.match(TEXTONLY["REGEX"], symbol):
-			print(TEXTONLY["ERROR"])
-		else:
-			break
-	while True:
-		exchange = input("Please enter exchange: ")
-		if not re.match(TEXTONLY["REGEX"], exchange):
-			print(TEXTONLY["ERROR"])
-		else:
-			break
-	if exchange == 'XNAS' or exchange == 'xnas':
-		ticker = symbol
-	else:
-		ticker = symbol + '.' + exchange
-	print(ticker + ' entered.')
-	return ticker
+def printExchanges():
+	api_response = api.getAncillaryData('exchanges')
+	if api.validateResponse(api_response):
+		for exchange in api_response["data"]:
+			if exchange["name"] != 'INDEX':
+				print('Name:' + exchange["name"] + ' | '
+					'Acronym:' + exchange["acronym"] + ' | '
+					'Code:' + exchange["mic"])
 
-def printExchanges(exchanges):
-	for exchange in exchanges:
-		if exchange["name"] != 'INDEX':
-			print('Name:' + exchange["name"] + ' | '
-				'Acronym:' + exchange["acronym"] + ' | '
-				'Code:' + exchange["mic"])
+def makeform(root):
+	entries = {}
+	for field in MENU_CONSTANTS["FIELDS"]:
+		print(field)
+		row = tk.Frame(root)
+		lab = tk.Label(row, width=22, text=field+": ", anchor='w')
+		ent = tk.Entry(row)
+		ent.insert(0, "")
+		row.pack(side=tk.TOP,
+				fill=tk.X,
+				padx=5,
+				pady=5)
+		lab.pack(side=tk.LEFT)
+		ent.pack(side=tk.RIGHT,
+				expand=tk.YES,
+				fill=tk.X)
+		entries[field] = ent
+	return entries
 
-def showOptions():
-	for option in MENU_CONSTANTS["OPTIONS"] :
-		print(option["key"] + ': ' + option["message"])
-
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+	root = tk.Tk()
+	ents = makeform(root)
+	b1 = tk.Button(root, text='Add Ticker',
+			command=(lambda e=ents: inputTicker(e)))
+	b1.pack(side=tk.LEFT, padx=5, pady=5)
+	b2 = tk.Button(root, text='Print Exchanges',
+			command=(lambda e=ents: printExchanges()))
+	b2.pack(side=tk.LEFT, padx=5, pady=5)
+	b3 = tk.Button(root, text='Quit', command=root.quit)
+	b3.pack(side=tk.LEFT, padx=5, pady=5)
+	root.mainloop()
