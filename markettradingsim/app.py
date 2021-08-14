f1b104d764cba52e22d98b984cceac5a9533b067
cooperbrindle <cooperbrindle96@gmail.com>
2021-08-14 22:12:11 +1000

    add auth

--- markettradingsim/app.py
+++ markettradingsim/app.py
@@ -1,62 +1,98 @@
-import re
-from markettradingsim.constants import MENU_CONSTANTS, TEXTONLY
-import markettradingsim.marketStack as api
-import markettradingsim.databaseActions as DB
-import flask
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from os import environ as env
+from functools import wraps
+from dotenv import load_dotenv, find_dotenv
+from flask import render_template, redirect, url_for, request, session, jsonify
+from werkzeug.exceptions import HTTPException
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
 
-app = flask.Flask(__name__)
+import flask
+import markettradingsim.mainFunc as main
+import markettradingsim.constants as constants
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+
+app = flask.Flask(__name__, static_url_path='/public', static_folder='./public')
 app.config["DEBUG"] = True
+app.secret_key = constants.SECRET_KEY
+
+@app.errorhandler(Exception)
+def handle_auth_error(ex):
+    response = jsonify(message=str(ex))
+    response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
+    return response
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    api_base_url=AUTH0_BASE_URL,
+    access_token_url=AUTH0_BASE_URL + '/oauth/token',
+    authorize_url=AUTH0_BASE_URL + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if constants.PROFILE_KEY not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+
+    return decorated
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+	if request.method == 'POST':
+		return redirect('/dashboard')
+	return render_template('home.html')
+
+@app.route('/callback')
+def callback_handling():
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    session[constants.JWT_PAYLOAD] = userinfo
+    session[constants.PROFILE_KEY] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@requires_auth
+def dashboard():
 	error = None
 	if request.method == 'POST':
 		if request.form['submit'] == 'add-ticker':
-			inputTicker(request.form)
+			main.inputTicker(request.form)
 		elif request.form['submit'] == 'get-exchanges':
-			printExchanges()
+			main.printExchanges()
 		else:
 			pass
-	return render_template('home.html', error=error)
+	return render_template('dashboard.html', error=error)
 
-# Route for handling the login page logic
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid Credentials. Please try again.'
-		else:
-			return redirect(url_for('home'))
-	return render_template('login.html', error=error)
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
 
-def inputTicker(entries):
-	stockCode = entries['Stock Code'].upper()
-	exchange = entries['Exchange'].upper()
-	if re.match(TEXTONLY["REGEX"], exchange) and re.match(TEXTONLY["REGEX"], stockCode):
-		if exchange == 'XNAS':
-			ticker = stockCode
-		else:
-			ticker = stockCode + '.' + exchange
-		addTicker(ticker)
-	else:
-		print(TEXTONLY["ERROR"])
-
-def addTicker(ticker):
-	api_response = api.getTicker(ticker, 'eod')
-	if api.validateResponse(api_response):
-		key = DB.checkProfile(api_response["data"]["symbol"])
-		if key == False:
-			key = DB.createProfile(api_response["data"])
-		DB.insertData(api_response["data"]["eod"], key)
-
-def printExchanges():
-	api_response = api.getAncillaryData('exchanges')
-	if api.validateResponse(api_response):
-		for exchange in api_response["data"]:
-			if exchange["name"] != 'INDEX':
-				print('Name:' + exchange["name"] + ' | '
-					'Acronym:' + exchange["acronym"] + ' | '
-					'Code:' + exchange["mic"])
+@app.route('/logout')
+def logout():
+    session.clear()
+    params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
